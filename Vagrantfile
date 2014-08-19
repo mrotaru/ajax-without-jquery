60a2635ce1c60eb181d8d9388575a34a67b24a60
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-08-19 19:06:43 +0100

    mk src and test dirs, mk Vagrantfile

---
+++ Vagrantfile
@@ -0,0 +1,14 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "ubuntu-14-20140818"
+  config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/20140818/trusty-server-cloudimg-amd64-vagrant-disk1.box"
+  config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.synced_folder "src/", "/vagrant/test/src"
+  config.vm.provision "shell",
+      inline: "sudo apt-get update && sudo apt-get -y install npm && sudo apt-get -y install nodejs"
+end
---
+++ test/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname);
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/get/json', function(req,res){
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ a: 1 }));
+});
+
+app.post('/post/json', function(req,res){
+  console.log(req.body);
+});
+
+app.post('/post/uri', function(req,res){
+  console.log(req.body);
+});
+app.listen(80);
